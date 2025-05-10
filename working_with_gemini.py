bae68ced04be670dd02427cc0e02738a9eb10324
tejoess <tejatsutarofc@gmail.com>
2025-05-10 18:07:15 +0530

    A simple chatbot

--- working_with_gemini.py
+++ working_with_gemini.py
@@ -1,5 +1,5 @@
 from google import genai
-from google.genai import types
+from google.genai import types # type: ignore
 import json
 from dotenv import load_dotenv
 import os
@@ -7,15 +7,27 @@ import os
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash',
-    
-    config=types.GenerateContentConfig(
-        system_instruction='You are a subjective answer evaluator and your task is to assign marks to given answer out of 2',
+history = []
 
-        max_output_tokens=500,
-        temperature=1
-    ),
-    contents=['AI is type of intelligence made by human which mimic human intelligence like learning, pattern identification, deicision making etc']
-)
-print(response.text)
+system_instruction='You are Kukie, a playful cat assistant who loves to chat and tell jokes. Be friendly and funny.',
+while True:
+    user_input = input('You: ')
+
+    if user_input.lower() in ['exit','quit','stop']:
+        print('Session end')
+        break
+    history.append(types.Content(role='user',parts=[types.Part(text=user_input)]))
+    response = client.models.generate_content(
+        model='gemini-2.0-flash',
+        
+        config=types.GenerateContentConfig(
+            system_instruction=system_instruction,
+            max_output_tokens=300,
+            temperature=0.7
+        ),
+        contents=history
+    )
+    reply = response.candidates[0].content.parts[0].text
+    print("Kukie:",reply)
+
+    history.append(types.Content(role='model',parts=[types.Part(text=reply)]))
