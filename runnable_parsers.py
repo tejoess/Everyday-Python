bdc6a402dab09f7a9f510017250ca4d199fc5b00
Tejas Sutar <tejassutarofc@gmail.com>
2025-06-13 11:12:58 +0530

    updated

---
+++ runnable_parsers.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(), override=True)
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence, RunnablePassthrough, RunnableParallel
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+model = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the folling joke - {text}',
+    input_variables=['text']
+)
+
+gen_chain = RunnableSequence(prompt1,model,parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+final_chain = RunnableSequence(gen_chain,parallel_chain)
+print(final_chain.invoke({'topic':'Virat kohli'}))
